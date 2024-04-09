12490ee79189a721a2d9174ea0195b72a3b81902
monica255 <monicasucianto123@gmail.com>
2024-04-10 03:17:26 +0800

    Initial commit

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
-
+import preprocess
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
-
-import preprocess
 
 class status (Resource):
     def get(self):
@@ -15,18 +13,17 @@ class status (Resource):
         except:
             return {'data': 'An Error Occurred during fetching Api'}
 
-
 class Sum(Resource):
     def get(self):
         data = request.json
-            # Extract the 'text' field
+        # Extract the 'text' field
         text = data.get('text', 'Default Text')
         text = preprocess.preprocess_text(text)
         return jsonify({'data': text})
-
 
 api.add_resource(status, '/')
 api.add_resource(Sum, '/api')
 
 if __name__ == '__main__':
     app.run()
+
--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,6 @@
 import numpy as np
 import nltk
 nltk.download('punkt')
-import re
 
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 import string
@@ -30,26 +29,23 @@ def sentence_weight(data,wordfreq):
     return weights
 
 def preprocess_text(text):
-    # Remove symbols
-    # text = re.sub(r'[^\w\s]', '', text)
-
     # Remove question words in Indonesian
     question_words = ['bagaimana', 'apa', 'dimana', 'siapa', 'kapan', 'berapa', 'mengapa','itu']
     text = ' '.join(word for word in text.split() if word.lower() not in question_words)
 
-    # create stemmer
+    # Create stemmer
     factory = StemmerFactory()
     stemmer = factory.create_stemmer()
-    # stemming process
+    # Stemming process
     text   = stemmer.stem(text)
 
-    # case folding
+    # Case folding
     text = text.lower()
 
-    # remove punctuation
+    # Remove punctuation
     text = text.translate(str.maketrans("","",string.punctuation))
 
-    # remove white space
+    # Remove white space
     text = text.strip()
 
     return text
@@ -58,14 +54,49 @@ def sum(text):
     data = sentence_split(text)
     wordfreq = word_freq(data)
     rank = sentence_weight(data,wordfreq)
-    print("aa",text)
+    # print("aa",text)
     n = 2
     result = ''
     sort_list = np.argsort(rank)[::-1][:n]
     for i in range(n):
         result += '{} '.format(data[sort_list[i]])
 
-    print(result)
+    # print(result)
     return result
 
-print(preprocess_text("apa itu hama"))
+print(sum("""Tanaman Jagung merupakan salah satu komoditas tanaman yang memiliki peran penting untuk pemenuhan kebutuhan pangan manusia, selain itu juga banyak dimanfaatkan sebagai bahan dasar pembuatan pakan ternak. Dengan kondisi tersebut menjadikan peluang bagi petani jagung untuk mengembangkan budidaya tanaman jagung agar produksi jagung meningkat.
+
+Salah satu kendala yang dihadapi bertanam jagung adalah adanya organisme penganggu tumbuhan (OPT). Saat ini OPT yang mulai menganggu produktivitas jagung adalah ulat grayak (Spodoptera frugiperda). Hama tersebut merupakan hama asli daerah tropis dari Amerika Serikat hingga Argentina. Ulat FAW dapat menyerang lebih dari 80 spesies tanaman, termasuk jagung, padi, tebu, sayuran, dan kapas. FAW dapat mengakibatkan kehilangan hasil yang signifikan apabila tidak ditangani dengan baik. Di Indonesia sendiri hama ulat grayak telah ditemukan pada beberapa lokasi pertanaman jagung di Aceh, Sumatera Utara, Sumatera Barat, Lampung dan Sumatera Selatan.
+
+
+Penyebaran hama ulat ‘FAW’ memang begitu cepat, hingga menimbulkan dampak serius yang sangat merugikan. Mengenalinya dengan lebih detail dan kemudian melakukan upaya pengendalian yang maksimal menjadi solusi yang baik untuk mengatasi masalah serangan hama yang sangat rakus tersebut.
+
+Gejala serangan Ulat FAW
+• Adanya bekas gerekan dari ulat
+• Pada permukaan atas daun atau disekitar pucuk tanaman jagung, ditemukan serbuk kasar seperti serbuk gergaji.
+• Ketika populasi ulat FAW ini sangat tinggi, maka bagian tongkol jagung juga akan diserang oleh hama ini.
+
+Cara untuk menanggulangi ulat grayak
+
+Rotasi tanaman untuk memutus daur hidup hama;
+Pengolahan tanah yang baik (selama 1 bulan) untuk mengangkat kepompong hama dari dalam tanah agar mati terjemur oleh sinar matahari;
+Pemasangan perangkap berferomon yaitu feromon Exi sebanyak 20 buah per hektar;
+Pemasangan lampu perangkap sebanyak 30 buah per hektar;
+Penyemprotan insektisida jika kerusakan daun telah mencapai 5%;
+Penyemprotan insektisida jika populasi kelompok telur telah mencapai 1 kelompok atau 10 tanaman;
+Selain itu pengendalian ulat grayak pada jagung juga dapat dilakukan dengan pembuatan pestisida nabati (botani).
+
+
+Pestisdia nabati untuk ulat grayak
+
+Bahan-bahan:
+
+Bubuk cabai 1 sendok teh
+Bawang putih 1 siung
+Bawang merah 1 butir
+Air 1 liter
+Deterjen 1 sendok teh.
+Cara pembuatan :
+
+Bawang putih dan bawang merah dihancurkan;
+Campur bubuk cabai dan air, aduk hingga rata, rendam 1 jam lalu saring, tambah deterjen aduk rata."""))
